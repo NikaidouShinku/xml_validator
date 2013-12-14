1d6ca04c21c1e3d4b4a6c2de178a4ea18772df27
NikaidouShinku <NikaidouShinku@icloud.com>
2013-12-14 17:42:28 +0800

    stack.c

---
+++ stack.c
@@ -0,0 +1,105 @@
+//
+//  Stack.c
+//  xml
+//
+//  Created by Hsieh Yu Hua on 2013/12/14.
+//  Copyright (c) 2013年 Hsieh Yu Hua_NTHU_s102062210. All rights reserved.
+#include"Stack.h"
+#include<stdlib.h>
+#include<string.h>
+/*构造一个空栈*/
+Stack *InitStack()
+{
+    Stack *ps = (Stack *)malloc(sizeof(Stack));
+    if(ps!=NULL)
+    {
+        ps->top = NULL;
+        ps->size = 0;
+    }
+    return ps;
+}
+
+/*判定是否为空栈*/
+int IsEmpty(Stack *ps)
+{
+    if(ps->top == NULL && ps->size == 0)
+        return 1;
+    else
+        return 0;
+}
+
+/*返回栈大小*/
+int GetSize(Stack *ps)
+{
+    return ps->size;
+}
+
+/*元素入栈*/
+PNode Push(Stack *ps,Item item)
+{
+    PNode pnode = (PNode)malloc(sizeof(Node));
+    if(pnode != NULL)
+    {
+        strcpy(pnode->data,item);
+        pnode->down = GetTop(ps,NULL);
+        ps->size++;
+        ps->top = pnode;
+        
+    }
+    return pnode;
+}
+
+/*返回栈顶元素*/
+PNode GetTop(Stack *ps,Item *pitem)
+{
+    if(IsEmpty(ps)!=1&&pitem!=NULL)
+    {
+        strcpy(*pitem,ps->top->data);
+    }
+    return ps->top;
+}
+
+
+/*元素出栈*/
+PNode Pop(Stack *ps,Item *pitem)
+{
+    PNode p = ps->top;
+    if(IsEmpty(ps)!=1&&p!=NULL)
+    {
+        if(pitem!=NULL)
+            strcpy(*pitem,p->data);
+        ps->size--;
+        ps->top = ps->top->down;
+        free(p);
+    }
+    return ps->top;
+}
+
+/*销毁一个栈*/
+void DestroyStack(Stack *ps)
+{
+    if(IsEmpty(ps)!=1)
+        ClearStack(ps);
+    free(ps);
+}
+
+/*把栈置空*/
+void ClearStack(Stack *ps)
+{
+    while(IsEmpty(ps)!=1)
+    {
+        Pop(ps,NULL);
+    }
+}
+
+/*遍历栈并访问visit函数 */
+void StackTraverse(Stack *ps,void (*visit)())
+{
+    PNode p = ps->top;
+    int i = ps->size;
+    while(i--)
+    {
+        visit(p->data);
+        p = p->down;
+    }
+}
